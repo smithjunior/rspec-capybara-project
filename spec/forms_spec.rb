c571a4c76f6814160f968dbfd68e9201bb7e1e08
Smith Júnior <smith.junior@icloud.com>
2019-11-16 15:37:55 -0300

    Added new scenarios

--- spec/forms_spec.rb
+++ spec/forms_spec.rb
@@ -1,5 +1,7 @@
-describe "Forms" do
-  it "login com sucesso" do
+# frozen_string_literal: true
+
+describe 'Forms' do
+  it 'login com sucesso' do
     visit 'https://training-wheels-protocol.herokuapp.com/login'
     # Fill fields, fill_in works with name and id propertier on HTML
     fill_in 'userId', with: 'stark'
@@ -8,6 +10,33 @@ describe "Forms" do
     click_button 'Login'
 
     expect(find('#flash').visible?).to be true
+
+    expect(find('#flash').text).to include 'Olá, Tony Stark. Você acessou a área logada!'
   end
-  
+
+  it 'senha incorreta' do
+    visit 'https://training-wheels-protocol.herokuapp.com/login'
+    # Fill fields, fill_in works with name and id propertier on HTML
+    fill_in 'userId', with: 'stark'
+    fill_in 'password', with: 'jarvis#'
+    # submit form
+    click_button 'Login'
+
+    expect(find('#flash').visible?).to be true
+
+    expect(find('#flash').text).to include 'Senha é invalida!'
+  end
+
+  it 'usuário não cadastrado' do
+    visit 'https://training-wheels-protocol.herokuapp.com/login'
+    # Fill fields, fill_in works with name and id propertier on HTML
+    fill_in 'userId', with: 'smith'
+    fill_in 'password', with: 'test!'
+    # submit form
+    click_button 'Login'
+
+    expect(find('#flash').visible?).to be true
+
+    expect(find('#flash').text).to include 'O usuário informado não está cadastrado!'
+  end
 end
