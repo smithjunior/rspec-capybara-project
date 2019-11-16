e5db93490dbfe50154b5831b03af19c3d5fe659d
Smith Júnior <smith.junior@icloud.com>
2019-11-16 16:06:01 -0300

    Created the dropdown tests

---
+++ README.md
@@ -0,0 +1,19 @@
+# Capybara + Rspec
+
+## Install project
+
+```bash
+$ bundle install
+```
+
+## Running tests
+
+```bash
+$ rspec -fd 
+```
+
+## Running testes by tag
+
+```bash
+$ rspec -fd --tag @dropdown
+```
---
+++ spec/dropdown_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+describe 'Caixa de seleção', :dropdown do
+  it 'item especifico simples ' do
+    visit 'https://training-wheels-protocol.herokuapp.com/dropdown'
+    select('Loki', from: 'dropdown')
+    sleep 2 # tempoarário
+  end
+
+  it 'item especifico com o find' do
+    visit 'https://training-wheels-protocol.herokuapp.com/dropdown'
+    drop = find('.avenger-list')
+    drop.find('option', text: 'Scott Lang').select_option
+    sleep 2 # tempoarário
+  end
+
+  it 'qualquer item' do
+    visit 'https://training-wheels-protocol.herokuapp.com/dropdown'
+    drop = find('.avenger-list')
+    drop.all('option').sample.select_option
+    sleep 2 # tempoarário
+  end
+end
