5671acb6267a070f7f27dd38f9a223f55ac7c0e8
Smith Júnior <smith.junior@icloud.com>
2019-11-16 18:28:06 -0300

    Added hover tests

---
+++ spec/hover_spec.rb
@@ -0,0 +1,27 @@
+describe 'Mouse Hover', :hover do
+  before(:each) do
+    visit 'https://training-wheels-protocol.herokuapp.com/hovers'
+  end
+
+  it 'quando passa o mouse sobre o blade' do
+    card = find('img[alt*=Blade]')
+    card.hover
+
+    expect(page).to have_content 'Nome: Blade'
+  end
+
+  it 'quando passa o mouse sobre o pantera negra' do
+    card = find('img[alt^=Pantera]')
+    card.hover
+    
+    expect(page).to have_content 'Nome: Pantera Negra'
+  end
+
+  it 'quando passa o mouse sobre o homem aranha' do
+    card = find('img[alt$=Aranha]')
+    card.hover
+    
+    expect(page).to have_content 'Nome: Homem Aranha'
+  end
+  
+end
