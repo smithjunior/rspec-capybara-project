aba19799ae274257e4fd45f7de7cde184d2439b4
Smith Júnior <smith.junior@icloud.com>
2019-11-17 19:37:54 -0300

    Added dynamics ids tests

---
+++ spec/id_dinamico_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+describe 'IDs Dinamicos', :dynamic_ids do
+  before(:each) do
+    visit 'https://training-wheels-protocol.herokuapp.com/access'
+  end
+
+  it 'cadastro' do
+    find('input[id$=UsernameInput]').set 'smith'
+    find('input[id^=PasswordInput]').set '123456'
+    find('a[id*=GetStartedButton]').click
+
+    expect(page).to have_content 'Dados enviados. Aguarde aprovação do seu cadastro!'
+  end
+end
